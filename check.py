3c029b5148f8ab2f15ba9363ad4fcce7ed84ed72
KatsuragiCSL <shue12345678@gmail.com>
2019-05-06 23:39:12 -0400

    Rewritten with argparse. Going to add more features

--- check.py
+++ check.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 
-import requests, sys, socket
+import requests, sys, socket, argparse
 from multiprocessing.dummy import Pool
 from urllib.parse import urlparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', help='domain you want to exclude', dest='check', required=True)
+parser.add_argument('-w', help='wordlist of domains', dest='domains', required=True)
+parse.add_argument('-t', help='number of threads', dest='thread')
+
+args = parser.parse_args()
+check = args.check[0]
+domains = args.domains[0]
+thread = args.thread[0]
+
+check_list = {}
+pool = Pool(thread)
 
 def hostname_resolves(hostname):
     try:
@@ -11,20 +24,10 @@ def hostname_resolves(hostname):
     except socket.error:
         return 0
 
-check = sys.argv[1]
-domains = sys.argv[2]
-pool = Pool(10)
-check_list = {}
-
 def checkRedirectDomain(domain):
         if hostname_resolves(domain.strip()):
             try:
-                #r = requests.get("http://" + domain.strip(), timeout = 3.05, allow_redirects = False)
                 r = requests.get("http://" + domain.strip(), timeout = 3.05)
-                #if ("Location" in r.headers):
-                 #   check_list[domain.strip()] = r.headers['Location']
-                #else:
-                 #   check_list[domain.strip()] = domain.strip()
                 check_list[domain.strip()] = r.url
             except:
                 pass
@@ -36,5 +39,5 @@ with open(domains) as f:
     pool.join()
     for x in check_list:
         parse = urlparse(check_list[x])
-        if (o.path != '/' or o.netloc != check):
+        if (parse.path != '/' or parse.netloc != check) :
             print(x + "\t" + "Target:" + check_list[x])
