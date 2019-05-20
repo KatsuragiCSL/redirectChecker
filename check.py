26cf58952fd541b47c9b1c1f9166bf3c867cdc84
KatsuragiCSL <shue12345678@gmail.com>
2019-05-20 05:30:05 -0400

    automatically rinse input now

--- check.py
+++ check.py
@@ -28,20 +28,20 @@ def hostname_resolves(hostname):
         return 0
 
 def checkRedirectDomain(domain):
-        if hostname_resolves(domain.strip()):
-            try:
-                r = requests.get("http://" + domain.strip(), timeout = 3.05)
-                check_list[domain.strip()] = r.url
-            except:
-                pass
+    if hostname_resolves(domain.strip()):
+        try:
+            r = requests.get("http://" + domain.strip(), timeout = 3.05)
+            check_list[domain.strip()] = r.url
+            parse = urlpase(r.url)
+            if (parse.path !='!' or parse.netloc != check):
+                print(domain.strip() + "\t" + "Target:" + check_list[x])
+        except:
+            pass
 
 
 with open(domains) as f:
     domain_list = f.readlines()
-    pool.map(checkRedirectDomain, domain_list)
+    rinse = [urlparse(x).netloc for x in domain_list if '://' in x] + [x for x in domain_list if '://' not in x]
+    pool.map(checkRedirectDomain, rinse)
     pool.close()
     pool.join()
-    for x in check_list:
-        parse = urlparse(check_list[x])
-        if (parse.path != '/' or parse.netloc != check) :
-            print(x + "\t" + "Target:" + check_list[x])
